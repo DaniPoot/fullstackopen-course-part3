6759245d0da407267166f4ca51507b92cf870192
DaniPoot <danielpoot.uc2@gmail.com>
2023-01-28 10:05:26 -0600

    Finish step 3, exercise 3.3

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require('express')
 const app = express()
 
+const persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
 app.get('/info', (request, response) => {
   const date = new Date()
   response.send(`
@@ -10,28 +33,16 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json([
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-  ])
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  console.log(id)
+  const person = persons.find(person => person.id == id)
+
+  if (!person) response.status(404).end()
+  response.json(person)
 })
 
 const PORT = 3001
